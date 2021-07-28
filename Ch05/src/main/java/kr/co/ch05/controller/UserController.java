e73b1fc16b756bd6281f1c0fca74ea3b20cb1c80
kim_3838 <kim_3838@naver.com>
2021-07-28 17:28:31 +0900

    Ch05

---
+++ Ch05/src/main/java/kr/co/ch05/controller/UserController.java
@@ -0,0 +1,75 @@
+package kr.co.ch05.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import kr.co.ch05.service.UserService;
+import kr.co.ch05.vo.UserVO;
+
+@Controller
+public class UserController {
+	
+	@Inject
+	private UserService service;
+	
+
+	@GetMapping("/user/list")
+	public String list(Model model) {
+		
+		List<UserVO> users = service.selectUsers();
+		
+		// 공유객체 model로 View에서 users를 참조 
+		model.addAttribute("users", users);
+		
+		return "/user/list";
+	}
+	
+	@GetMapping("/user/register")
+	public String register() {
+		return "/user/register";
+	}
+	
+	@PostMapping("/user/register")
+	public String register(UserVO vo) {
+		
+		service.insertUser(vo);
+		
+		return "/user/register";
+	}
+	
+	
+	@GetMapping("/user/modify")
+	public String modify(String uid, Model model) {
+		
+		UserVO user = service.selectUser(uid);
+		
+		model.addAttribute("user", user);
+		
+		return "/user/modify";
+	}
+	
+	@PostMapping("/user/modify")
+	public String modify(UserVO vo) {
+		
+		service.updateUser(vo);
+		
+		return "redirect:/user/list";
+	}
+	
+	@GetMapping("/user/delete")
+	public String delete(String uid) {
+		
+		service.deleteUser(uid);
+		
+		return "redirect:/user/list";
+	}
+	
+	
+	
+}
---
+++ Ch05/src/main/java/kr/co/ch05/dao/UserDAO.java
@@ -0,0 +1,44 @@
+package kr.co.ch05.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.co.ch05.vo.UserVO;
+
+@Repository
+public class UserDAO {
+
+	@Autowired
+	private SqlSessionTemplate mybatis;
+	
+	public void insertUser(UserVO vo) {
+		mybatis.insert("mapper.user.INSERT_USER", vo);
+	}
+	
+	public UserVO selectUser(String uid) {
+		return mybatis.selectOne("mapper.user.SELECT_USER", uid);
+	}
+	
+	public List<UserVO> selectUsers() {
+		return mybatis.selectList("mapper.user.SELECT_USERS");
+	}
+	
+	public void updateUser(UserVO vo) {
+		mybatis.update("mapper.user.UPDATE_USER", vo);
+	}
+	
+	public void deleteUser(String uid) {
+		mybatis.delete("mapper.user.DELETE_USER", uid);
+	}
+}
+
+
+
+
+
+
+
+
