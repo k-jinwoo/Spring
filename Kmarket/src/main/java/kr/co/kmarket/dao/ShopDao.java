cb2fbcd4556eefbffa24c309f75880d9c8e943f7
kim_3838 <kim_3838@naver.com>
2021-07-28 17:30:04 +0900

    2021/07/28 Spring Kmarket 진행 중

---
+++ Kmarket/src/main/java/kr/co/kmarket/dao/ShopDao.java
@@ -0,0 +1,29 @@
+package kr.co.kmarket.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import kr.co.kmarket.vo.CartVo;
+import kr.co.kmarket.vo.OrderVo;
+import kr.co.kmarket.vo.ProductVo;
+
+@Repository
+public interface ShopDao {
+
+	public void insertProduct(ProductVo vo);
+	public ProductVo selectProduct(int code);
+	public List<ProductVo> selectProducts(int cate1, int cate2, String sort);
+	public ProductVo selectTitles(int cate1, int cate2);
+	public void updateProduct(int code);
+	public void deleteProduct(int code);
+	
+	public int selectCountCart(int code, String uid);
+	public void insertCart(CartVo vo);
+	public List<CartVo> selectCart(String uid);
+	
+	public void insertOrder(OrderVo vo);
+	public void insertOrderDetail(int orderId, int code);
+	
+	
+}
